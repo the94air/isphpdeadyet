9d2f2e498aacf33a3f6c1649c1d830e765ad7a1e
Abdalla Arbab <the94air@users.noreply.github.com>
2024-05-03 19:28:05 +0300

    feat: adding analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      script: [
+        {
+          defer: true,
+          src: "https://tics.abdalla.sd/script.js",
+          "data-website-id": "89698437-2520-44eb-af55-9eca17cb0d46",
+        },
+      ],
+    }
+  },
   css: [
     '@unocss/reset/tailwind.css',
     "~/assets/css/base.css"
